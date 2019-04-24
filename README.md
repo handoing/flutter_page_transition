ecf6b1368fe883cd07424a9bcbde341d1ad58887
chenhan1 <chenhan1@360.cn>
2019-04-24 15:00:51 +0800

    add custom

--- README.md
+++ README.md
@@ -2,13 +2,60 @@
 
 A new Flutter Page Transition package.
 
+[![Version](https://img.shields.io/badge/version-0.0.2-blue.svg)](https://github.com/handoing/flutter_page_transition)
+
 ## Getting Started
 
-This project is a starting point for a Dart
-[package](https://flutter.io/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+添加git仓库依赖:
+```yaml
+flutter_page_transition:
+  git:
+    url: git://github.com/handoing/flutter_page_transition.git
+```
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## Example
+
+使用PageRouteBuilder
+```dart
+onGenerateRoute: (RouteSettings routeSettings){
+    return new PageRouteBuilder<dynamic>(
+      settings: routeSettings,
+      pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+        switch (routeSettings.name){
+          case '/':
+            return HomePage();
+          case '/first':
+            return FirstPage();
+          case '/second':
+            return SecondPage();
+          default:
+            return null;
+        }
+      },
+      transitionDuration: const Duration(milliseconds: 300),
+      transitionsBuilder: (BuildContext context, Animation<double> animation,
+          Animation<double> secondaryAnimation, Widget child) {
+          return effectMap[PageTransitionType.slideInLeft](Curves.linear, animation, secondaryAnimation, child);
+      }
+    );
+}
+```
+
+使用自定义效果
+```dart
+transitionEffect.createCustomEffect(
+  handle: (Curve curve, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    return new SlideTransition(
+      position: new Tween<Offset>(
+        begin: const Offset(1.0, 0.0),
+        end: const Offset(0.0, 0.0),
+      ).animate(CurvedAnimation(parent: animation, curve: curve)),
+      child: child,
+    );
+  }
+);
+
+...
+
+effectMap[PageTransitionType.custom](Curves.linear, animation, secondaryAnimation, child);
+```
--- lib/flutter_page_transition.dart
+++ lib/flutter_page_transition.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 enum PageTransitionType {
 
+  custom, // 自定义过渡效果
+
   slideInLeft, // 从左向右
   slideInRight, // 从右向左
   slideInUp, // 从下向上
@@ -14,9 +16,33 @@ enum PageTransitionType {
   slideUp, // 从下向上
   slideDown, // 从上向下
 
+  slideParallaxLeft, // 从左向右
+  slideParallaxRight, // 从右向左
+  slideParallaxUp, // 从下向上
+  slideParallaxDown, // 从上向下
+
+  slideZoomLeft, // 从左向右
+  slideZoomRight, // 从右向左
+  slideZoomUp, // 从下向上
+  slideZoomDown, // 从上向下
+
 }
 
-class SlideTransitionEffect {
+class TransitionEffect {
+  Function _customEffect;
+
+  TransitionEffect() {
+    _customEffect = (Curve curve, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+      return child;
+    };
+  }
+
+  get customEffect => _customEffect;
+
+  createCustomEffect({Function handle}) {
+    _customEffect = handle;
+  }
+
   static createSlideIn(Tween tween) {
     return (Curve curve, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) => new SlideTransition(
       position: tween.animate(CurvedAnimation(parent: animation, curve: curve)),
@@ -32,7 +58,18 @@ class SlideTransitionEffect {
       ),
     );
   }
+  static createZoomSlide({Tween animationTween, Tween secondaryAnimationTween}) {
+    return (Curve curve, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) => new SlideTransition(
+      position: animationTween.animate(animation),
+      child: new ScaleTransition(
+        scale: secondaryAnimationTween.animate(secondaryAnimation),
+        child: child,
+      ),
+    );
+  }
 }
+
+TransitionEffect transitionEffect = TransitionEffect();
 
 Tween t1 = new Tween<Offset>(
   begin: const Offset(1.0, 0.0),
@@ -67,15 +104,50 @@ Tween t8 = new Tween<Offset>(
   end: const Offset(0.0, 1.0),
 );
 
-Map effectMap = <PageTransitionType, void>{
-  PageTransitionType.slideInLeft: SlideTransitionEffect.createSlideIn(t1),
-  PageTransitionType.slideInRight: SlideTransitionEffect.createSlideIn(t2),
-  PageTransitionType.slideInUp: SlideTransitionEffect.createSlideIn(t3),
-  PageTransitionType.slideInDown: SlideTransitionEffect.createSlideIn(t4),
-  PageTransitionType.slideLeft: SlideTransitionEffect.createSlide(animationTween: t1, secondaryAnimationTween: t5),
-  PageTransitionType.slideRight: SlideTransitionEffect.createSlide(animationTween: t2, secondaryAnimationTween: t6),
-  PageTransitionType.slideUp: SlideTransitionEffect.createSlide(animationTween: t3, secondaryAnimationTween: t7),
-  PageTransitionType.slideDown: SlideTransitionEffect.createSlide(animationTween: t4, secondaryAnimationTween: t8),
+Tween t9 = new Tween<Offset>(
+  begin: const Offset(0.0, 0.0),
+  end: const Offset(-0.8, 0.0),
+);
+Tween t10 = new Tween<Offset>(
+  begin: const Offset(0.0, 0.0),
+  end: const Offset(0.8, 0.0),
+);
+Tween t11 = new Tween<Offset>(
+  begin: const Offset(0.0, 0.0),
+  end: const Offset(0.0, -0.8),
+);
+Tween t12 = new Tween<Offset>(
+  begin: const Offset(0.0, 0.0),
+  end: const Offset(0.0, 0.8),
+);
+
+Tween t13 = new Tween<double>(
+  begin: 1.0,
+  end: 0.9,
+);
+
+final Map effectMap = <PageTransitionType, void>{
+  PageTransitionType.custom: transitionEffect.customEffect,
+
+  PageTransitionType.slideInLeft: TransitionEffect.createSlideIn(t1),
+  PageTransitionType.slideInRight: TransitionEffect.createSlideIn(t2),
+  PageTransitionType.slideInUp: TransitionEffect.createSlideIn(t3),
+  PageTransitionType.slideInDown: TransitionEffect.createSlideIn(t4),
+
+  PageTransitionType.slideLeft: TransitionEffect.createSlide(animationTween: t1, secondaryAnimationTween: t5),
+  PageTransitionType.slideRight: TransitionEffect.createSlide(animationTween: t2, secondaryAnimationTween: t6),
+  PageTransitionType.slideUp: TransitionEffect.createSlide(animationTween: t3, secondaryAnimationTween: t7),
+  PageTransitionType.slideDown: TransitionEffect.createSlide(animationTween: t4, secondaryAnimationTween: t8),
+
+  PageTransitionType.slideParallaxLeft: TransitionEffect.createSlide(animationTween: t1, secondaryAnimationTween: t9),
+  PageTransitionType.slideParallaxRight: TransitionEffect.createSlide(animationTween: t2, secondaryAnimationTween: t10),
+  PageTransitionType.slideParallaxUp: TransitionEffect.createSlide(animationTween: t3, secondaryAnimationTween: t11),
+  PageTransitionType.slideParallaxDown: TransitionEffect.createSlide(animationTween: t4, secondaryAnimationTween: t12),
+
+  PageTransitionType.slideZoomLeft: TransitionEffect.createZoomSlide(animationTween: t1, secondaryAnimationTween: t13),
+  PageTransitionType.slideZoomRight: TransitionEffect.createZoomSlide(animationTween: t2, secondaryAnimationTween: t13),
+  PageTransitionType.slideZoomUp: TransitionEffect.createZoomSlide(animationTween: t3, secondaryAnimationTween: t13),
+  PageTransitionType.slideZoomDown: TransitionEffect.createZoomSlide(animationTween: t4, secondaryAnimationTween: t13),
 };
 
 class PageTransition extends PageRouteBuilder {
